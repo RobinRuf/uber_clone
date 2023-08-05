c9ab1336ccec2c84d09aa22974f7f8c8c23dfdae
Robin <robin@MacbookAir.local>
2023-08-05 16:18:22 +0200

    feat(redux): implemented dispatch of Origin
    Implemented the autocomplete function and the dispatch, which will set the Origin in the Data Layer
    (Redux) , after a button was pressed

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,8 +2,14 @@ import React from "react";
 import { StyleSheet, Image, Text, View, SafeAreaView } from "react-native";
 import tw from "tailwind-react-native-classnames";
 import NavOptions from "../components/NavOptions";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { GOOGLE_MAPS_APIKEY } from "@env";
+import { useDispatch } from "react-redux";
+import { setDestination, setOrigin } from "../slices/navSlice";
 
 const HomeScreen = () => {
+  const dispatch = useDispatch(); // used to shoot the action (reducer, selector) into the data layer (redux)
+
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
       <View style={tw`p-5`}>
@@ -15,6 +21,37 @@ const HomeScreen = () => {
           }}
           source={{
             uri: "https://links.papareact.com/gzs",
+          }}
+        />
+        <GooglePlacesAutocomplete
+          nearbyPlacesAPI="GooglePlacesSearch"
+          debounce={400}
+          placeholder="Where From?"
+          styles={{
+            container: {
+              flex: 0,
+            },
+            textInput: {
+              fontSize: 18,
+            },
+          }}
+          onPress={(data, details = null) => {
+            dispatch(
+              setOrigin({
+                location: details.geometry.location,
+                description: data.description,
+              })
+            );
+
+            dispatch(setDestination(null));
+          }}
+          fetchDetails={true}
+          returnKeyType={"search"}
+          minLength={2}
+          enablePoweredByContainer={false}
+          query={{
+            key: GOOGLE_MAPS_APIKEY,
+            language: "en",
           }}
         />
         <NavOptions />
