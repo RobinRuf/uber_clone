cd8ca2649cd111e5ab19e704e41f77dac849a892
Robin <robin@MacbookAir.local>
2023-08-05 22:04:23 +0200

    feat(map): create destination field and with a stack navigator the RideOptionsCard also

---
+++ components/NavigateCard.js
@@ -0,0 +1,65 @@
+import React from "react";
+import { StyleSheet, View, Text, SafeAreaView } from "react-native";
+import tw from "tailwind-react-native-classnames";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { GOOGLE_MAPS_APIKEY } from "@env";
+import { useDispatch } from "react-redux";
+import { setDestination } from "../slices/navSlice";
+import { useNavigation } from "@react-navigation/native";
+
+const NavigateCard = () => {
+  const dispatch = useDispatch();
+  const navigation = useNavigation();
+
+  return (
+    <SafeAreaView style={tw`bg-white flex-1`}>
+      <Text style={tw`text-center py-5 text-xl`}>Good Morning, Robin</Text>
+      <View style={tw`border-t border-gray-200 flex-shrink`}>
+        <View>
+          <GooglePlacesAutocomplete
+            placeholder="Where to?"
+            styles={toInputBoxStyles}
+            enablePoweredByContainer={false}
+            fetchDetails={true}
+            returnKeyType={"search"}
+            minLength={2}
+            onPress={(data, details = null) => {
+              dispatch(
+                setDestination({
+                  location: details.geometry.location,
+                  description: data.description,
+                })
+              );
+              navigation.navigate("RideOptionsCard");
+            }}
+            query={{
+              key: GOOGLE_MAPS_APIKEY,
+              language: "en",
+            }}
+            nearbyPlacesAPI="GooglePlacesSearch"
+            debounce={400}
+          />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default NavigateCard;
+
+const toInputBoxStyles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    paddingTop: 20,
+    flex: 0,
+  },
+  textInput: {
+    backgroundColor: "#DDDDDF",
+    borderRadius: 0,
+    fontSize: 18,
+  },
+  textInputContainer: {
+    paddingHorizontal: 20,
+    paddingBottom: 0,
+  },
+});
---
+++ components/RideOptionsCard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+
+const RideOptionsCard = () => {
+  return (
+    <View>
+      <Text>RideOptionsCard</Text>
+    </View>
+  );
+};
+
+export default RideOptionsCard;
