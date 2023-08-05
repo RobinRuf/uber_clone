e6d7c3ed4939066293324b1afd35b5820ae996d5
Robin <robin@MacbookAir.local>
2023-08-05 14:37:16 +0200

    feat(screens): created blank MapScreen, continued working on HomeScreen, added StackNavigator

---
+++ components/NavOptions.js
@@ -0,0 +1,54 @@
+import { useNavigation } from "@react-navigation/native";
+import React from "react";
+import { Text, View, FlatList, TouchableOpacity, Image } from "react-native";
+import { Icon } from "react-native-elements";
+import tw from "tailwind-react-native-classnames";
+
+const data = [
+  {
+    id: "123",
+    title: "Get a ride",
+    image: "https://links.papareact.com/3pn",
+    screen: "MapScreen",
+  },
+  {
+    id: "456",
+    title: "Order Food",
+    image: "https://links.papareact.com/28w",
+    screen: "EatsScreen", // Change in future
+  },
+];
+
+const NavOptions = () => {
+  const navigation = useNavigation();
+
+  return (
+    <FlatList
+      data={data}
+      keyExtractor={(item) => item.id}
+      horizontal
+      renderItem={({ item }) => (
+        <TouchableOpacity
+          style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}
+          onPress={() => navigation.navigate(item.screen)}
+        >
+          <View>
+            <Image
+              source={{ uri: item.image }}
+              style={{ width: 120, height: 120, resizeMode: "contain" }}
+            />
+            <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
+            <Icon
+              style={tw`p-2 bg-black rounded-full w-10 mt-4`}
+              name="arrowright"
+              color="white"
+              type="antdesign"
+            />
+          </View>
+        </TouchableOpacity>
+      )}
+    />
+  );
+};
+
+export default NavOptions;
