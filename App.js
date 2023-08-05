28cd6f9e4b2a9de4932f80f3093784bd94221c27
Robin <robin@MacbookAir.local>
2023-08-05 11:24:20 +0200

    fix(app): small displayed bug fixed

--- App.js
+++ App.js
@@ -3,14 +3,12 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { Provider } from "react-redux";
 import { store } from "./store";
+import HomeScreen from "./screens/HomeScreen";
 
 export default function App() {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
+      <HomeScreen />
     </Provider>
   );
 }
