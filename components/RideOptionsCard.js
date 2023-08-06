ee2689521bd781593d203f30669068c6cebc0795
Robin <robin@MacbookAir.local>
2023-08-06 13:31:16 +0200

    feat(price calculation): created the price calculation and implemented it into the price field

--- components/RideOptionsCard.js
+++ components/RideOptionsCard.js
@@ -35,6 +35,9 @@ const data = [
   },
 ];
 
+// Price Calculation
+const SURGE_CHARGE_RATE = 1.5;
+
 const RideOptionsCard = () => {
   const navigation = useNavigation();
   const [selected, setSelected] = useState(null);
@@ -72,7 +75,17 @@ const RideOptionsCard = () => {
               <Text style={tw`text-xl font-semibold`}>{title}</Text>
               <Text>{travelTimeInformation?.duration.text}</Text>
             </View>
-            <Text style={tw`text-xl`}>$19</Text>
+            <Text style={tw`text-xl`}>
+              {new Intl.NumberFormat("en-gb", {
+                style: "currency",
+                currency: "GBP",
+              }).format(
+                (travelTimeInformation?.duration.value *
+                  SURGE_CHARGE_RATE *
+                  multiplier) /
+                  100
+              )}
+            </Text>
           </TouchableOpacity>
         )}
       />
